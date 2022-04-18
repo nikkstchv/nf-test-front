5f834ef178704cc3332dca9c9ba4e203d2baebc1
Nikita Kostichev <telerific@gmail.com>
2022-04-18 16:21:05 +0300

    init

---
+++ src/screens/Orders/List/styles.m.styl.d.ts
@@ -0,0 +1,19 @@
+declare namespace StylesMStylNamespace {
+  export interface IStylesMStyl {
+    screen: string;
+    // fix
+    screenWrapper: string;
+    table: string;
+    head: string;
+    row: string;
+    body: string;
+    pagination: string;
+  }
+}
+
+declare const StylesMStylModule: StylesMStylNamespace.IStylesMStyl & {
+  /** WARNING: Only available when `css-loader` is used without `style-loader` or `mini-css-extract-plugin` */
+  locals: StylesMStylNamespace.IStylesMStyl;
+};
+
+export = StylesMStylModule;
